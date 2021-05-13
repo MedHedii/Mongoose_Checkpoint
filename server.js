f498f00aaf720a97507b8f4653bd40cad8821b30
MedHedii <79151270+MedHedii@users.noreply.github.com>
2021-05-13 23:11:20 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,134 @@
+// connect DB
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "./.env" });
+
+const Person = require("./model");
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("server is connected to BD"))
+    .catch((err) => console.log(err));
+
+//Create a person having this prototype:
+
+const newPerson = new Person({
+    name: "Mohamed",
+    age: 30,
+    favoriteFoods: ["Spaguetti", "Pizza", "Chicken"],
+});
+newPerson.save((err, data) => {
+    err ? console.log(err) : console.log(`${data}  is saved`);
+});
+
+// Create Many record in a Person Model
+
+Person.create(
+    [
+        {
+            name: "Mouna",
+            age: 28,
+            favoriteFoods: ["Pain au chocolat", "Panini"],
+        },
+
+        {
+            name: "Zayneb",
+            age: 30,
+            favoriteFoods: ["Omlette", "Banane"],
+        },
+
+        {
+            name: "Said",
+            age: 34,
+            favoriteFoods: ["Manga", "Kiwi"],
+        },
+    ],
+    (err) => {
+        err ? console.log(err) : console.log("Records are Added Successfuly");
+    }
+);
+
+// Find all the people having a given name
+
+Person.find({ name: "Cyrine" }, (err, data) => {
+    err
+        ? console.log(err)
+        : console.log("You find the person with name Cyrine");
+});
+
+//FindOne operation by favorite food
+
+Person.findOne({ favoriteFoods: "Panini" }, (err, data) => {
+    err ? console.log(err) : console.log(`${data.name} like Panini`);
+});
+
+// Find a Model by ID
+
+Person.findById("600c6f3a037e152664e19741", (err, data) => {
+    err ? console.log(err) : console.log("we find the person :" + data.name);
+});
+
+// Find and update a Model
+Person.findByIdAndUpdate(
+    "600c6f3a037e152664e19741",
+    { $push: { favoriteFoods: "hamburger" } },
+    (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            data.save((err, doc) => {
+                err
+                    ? console.log(err)
+                    : console.log("Data was Updated" + doc.favoriteFoods);
+            });
+        }
+    }
+);
+
+// Find One and Update
+
+Person.findOneAndUpdate(
+    { name: "Amine" },
+    { age: "20" },
+    { new: true },
+    (err, data) => {
+        err
+            ? console.log(err)
+            : console.log(
+                  "The age of " +
+                      data.name +
+                      " is updated to " +
+                      data.age +
+                      " years"
+              );
+    }
+);
+
+Person.find();
+
+// Find by Id and Delete the document
+
+Person.findByIdAndRemove("600c6f3a037e152664e19741", (err, data) => {
+    err ? console.log(err) : console.log(data._id + " Document was deleted");
+});
+
+//Delete Many Documents with model.remove()
+
+Person.remove({ name: "Mehdi" }, (err) => {
+    err
+        ? console.log(err)
+        : console.log("All document with name Mehdi are removed");
+});
+
+//Find people who like Banane
+
+Person.find({ favoriteFoods: "Manga" })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((err, data) => {
+        err ? console.log(err) : console.log(data);
+    });
